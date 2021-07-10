b1ba6d10280b00468aa7f1f767b8d3e38e3d3791
DrPayne25 <alex.payne1125@gmail.com>
2021-07-10 15:18:00 -0700

    Header Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from './Header'
 import './App.css';
 
 class App extends React.Component {
   render(){
     return(
-      <h1>Hello World</h1>
+      <>
+      <Header />
+      </>
     )
   }
 }
